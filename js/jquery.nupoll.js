fe8853ff45ac3e6bce240086a073e6523ac0f531
Danny <danlau168@gmail.com>
2015-12-31 15:27:56 -0800

    return this

--- js/jquery.nupoll.js
+++ js/jquery.nupoll.js
@@ -1,7 +1,31 @@
 ;(function($){
   $.fn.nupoll = function(){
     $("<h1/>", {
-      text: "New Poll"
+      text: "Sample Poll"
     }).appendTo(this);
+
+    var form = $("<form/>").appendTo(this);
+
+    var x, categories = ["A", "B", "C", "D"];
+
+    for (x = 0; x < categories.length; x++) {
+      $("<input/>", {
+        type: "radio",
+        name: "categories",
+        id: categories[x],
+        value: categories[x]
+      }).appendTo(form);
+
+      $("<label/>", {
+        text: categories[x],
+        "for": categories[x]
+      }).appendTo(form);
+    }
+
+    $("<button/>", {
+      text: "Answer!"
+    }).appendTo(form);
+
+    return this;
   };
 }(jQuery));
