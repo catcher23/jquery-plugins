3dca32aff67c0e386bc041260a915c01bdd023d0
Danny <danlau168@gmail.com>
2016-01-01 17:03:07 -0800

    disabled buttons

--- js/jquery.nupoll.js
+++ js/jquery.nupoll.js
@@ -1,31 +1,65 @@
 ;(function($){
-  $.fn.nupoll = function(){
+
+  var defaults = {
+    question: "Enter question here?",
+    url: "",
+    buttonText: "Answer!",
+    categories: ["A", "B", "C", "D"],
+    containerClass: "nupoll",
+    formClass: "nupoll-form",
+    buttonClass: "nupoll-submit"
+  };
+
+  function Nupoll(element, options) {
+    var widget = this;
+    widget.config = $.extend({}, defaults, options);
+    widget.element = element;
+
+    widget.element.on("submit", function(e) {
+      e.preventDefault();
+    });
+
+    widget.element.one("change", function(e) {
+      widget.element.find("button").prop("disabled", false);
+    });
+    this.init();
+  }
+
+  Nupoll.prototype.init = function() {
+    this.element.addClass(this.config.containerClass);
+
     $("<h1/>", {
-      text: "Sample Poll"
-    }).appendTo(this);
+      text: this.config.question
+    }).appendTo(this.element);
 
-    var form = $("<form/>").appendTo(this);
+    var form = $("<form/>").addClass(this.config.formClass).appendTo(this.element);
+    var x;
 
-    var x, categories = ["A", "B", "C", "D"];
-
-    for (x = 0; x < categories.length; x++) {
+    for (x = 0; x < this.config.categories.length; x++) {
       $("<input/>", {
         type: "radio",
         name: "categories",
-        id: categories[x],
-        value: categories[x]
+        id: this.config.categories[x],
+        value: this.config.categories[x]
       }).appendTo(form);
 
       $("<label/>", {
-        text: categories[x],
-        "for": categories[x]
+        text: this.config.categories[x],
+        "for": this.config.categories[x]
       }).appendTo(form);
     }
 
     $("<button/>", {
-      text: "Answer!"
+      text: this.config.buttonText,
+      "class": this.config.buttonClass,
+      disabled: "disabled"
     }).appendTo(form);
+  };
 
-    return this;
+
+
+  $.fn.nupoll = function(options){
+    new Nupoll(this.first(), options);
+    return this.first();
   };
 }(jQuery));
